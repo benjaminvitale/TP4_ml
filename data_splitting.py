b004cd41dcb996e07702048b05ac14defd7f026f
benjaminvitale <bvitale@udesa.edu.ar>
2024-10-14 11:51:58 -0300

    uu

--- data_splitting.py
+++ data_splitting.py
@@ -83,52 +83,5 @@ def data_create_test(cd,vals_min,vals_max):
     return x_test,y_test
 
 
-import numpy as np
-
-# Funciones auxiliares para calcular las métricas
-
-# Función para hacer k-fold cross-validation y calcular las métricas
-def k_fold_cross_validation(model, X, y, k,mae,rmse,r2_score):
-    # Shuffle y dividir los datos en k-folds
-    indices = np.arange(len(X))
-    np.random.shuffle(indices)
-    X_shuffled = X[indices]
-    y_shuffled = y[indices]
-    
-    fold_size = len(X) // k
-    folds_X = [X_shuffled[i * fold_size: (i + 1) * fold_size] for i in range(k)]
-    folds_y = [y_shuffled[i * fold_size: (i + 1) * fold_size] for i in range(k)]
-    
-    # Inicializar listas para guardar las métricas
-    mae_list = []
-    rmse_list = []
-    r2_list = []
-    
-    # Iterar sobre los folds
-    for i in range(k):
-        # Fold actual como conjunto de validación
-        X_val = folds_X[i]
-        y_val = folds_y[i]
-        
-        # Resto como conjunto de entrenamiento
-        X_train = np.vstack([folds_X[j] for j in range(k) if j != i])
-        y_train = np.hstack([folds_y[j] for j in range(k) if j != i])
-        if y_train.ndim != 1:
-            y_train = y_train.reshape(-1, 1)
-        # Entrenar el modelo con los datos de entrenamiento
-        model.train(X_train, y_train)
-        
-        # Predecir con los datos de validación
-        y_pred = model.predict(X_val)
-        
-        # Calcular las métricas
-        mae_list.append(mae(y_val, y_pred))
-        rmse_list.append(rmse(y_val, y_pred))
-        r2_list.append(r2_score(y_val, y_pred))
-    
-    # Retornar los promedios de las métricas
-    return [np.mean(mae_list), np.mean(rmse_list),np.mean(r2_list)]
-
-
 
 
