a6bf1c7ccd45325971b590b23c6ae9c5cbb6fd52
benjaminvitale <bvitale@udesa.edu.ar>
2024-10-23 16:21:21 -0300

    jjj

--- todo.py
+++ todo.py
@@ -4,15 +4,35 @@ import data_splitting as ds
 import curacion as cc
 import metrics as mc
 import pandas as pd
+import numpy as np
+from sklearn.neural_network import MLPRegressor
+from sklearn.datasets import make_regression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import StandardScaler
+
+
 def save_results():
     data_frame_dev = cc.cd
     data_frame_test = cc.data_2_frame
-    x_train,y_train,x_dev,y_dev,vals_min,vals_max = ds.data_split_dev(data_frame_dev)
-    x_test_2,y_test_2 = ds.data_create_test(data_frame_test,vals_min,vals_max)
-    X_kf,Y_kf = ds.data_create_test(data_frame_dev,vals_min,vals_max)
+   
+    
 
 
-    neural_net = md.MLP
-    neural_net.fit((x_train,y_train),10,200,0.1)
-    print(neural_net.evaluate((x_dev,y_dev)))
+  
+    
+    neural_net = md.NeuralNetwork([15,10,8,4,1],0.01)
+    neural_net.train(x_train.T,y_train.T,250,[15,10,8,4,1],0.01)
+    y_pred = np.array(neural_net.predict(x_dev.T))
+    y_pred.reshape(-1,1)
+    print(y_pred)
+    print(mc.RMSE(y_dev,y_pred))
+
+    
 save_results()
+
+x_train = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+y_train = np.array([[0], [1], [1], [0]])
+
+#print(x_train.shape)
+#print(y_train.shape)
